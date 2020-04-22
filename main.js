b2f6b4ce6ffc92902836c68a8b958f268e87d552
anuragkumar96 <s531372@nwmissouri.edu>
2020-04-22 13:49:51 -0500

    Added computed properties

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app=new Vue({
 el:'#app',
 data:{
+    brand:'Vue Mastery',
     product:'Socks',
-    image: './vmSocks-green-onWhite.jpg',
-    inStock: true,
+    selectedVarient:0,
     details:["80% cotton","20% polyster","Gender-neutral"],
     varients:[
         {
             varientId: 2234,
             varientColor: "green",
             varientImage: './vmSocks-green-onWhite.jpg',
+            varientQuantity: 0
         },
         {
             varientId: 2235,
             varientColor: "blue",
             varientImage: './vmSocks-blue-onWhite.jpg',
+            varientQuantity: 45
         }
     ],
     cart:0
@@ -27,8 +29,20 @@ methods:{
         this.cart-=1;
     }
     ,
-    updateProduct(varientImage){
-        this.image=varientImage
+    updateProduct(index){
+        this.selectedVarient=index
+        console.log(index)
+    }
+},
+computed:{
+    title(){
+        return this.brand+" "+this.product
+    },
+    image(){
+        return this.varients[this.selectedVarient].varientImage
+    },
+    inStock(){
+        return this.varients[this.selectedVarient].varientQuantity
     }
 }
 })
